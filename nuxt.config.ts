041debe1223c12fabb8c4da32e6c12bc952cd14e
FlameyFox <flameblader95@live.dk>
2022-04-03 13:46:14 +0200

    added charset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,7 @@ export default defineNuxtConfig({
     },
     title: "Flameyfox - Web Developer",
     meta: [
+      { charset: 'utf-8' },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { meta: "title", content: "Flameyfox" },
       {
