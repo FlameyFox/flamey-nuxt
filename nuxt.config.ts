ea185d58acbea29105cbe1d94e78d2af4ba43720
FlameyFox <bjarke@bjarkeef.com>
2022-11-16 17:51:17 +0100

    changed to bunny fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,11 +30,10 @@ export default defineNuxtConfig({
     ],
     link: [
       { rel: "canonical", href: "https://flameyfox.com" },
-      { rel: "preconnect", href: "https://fonts.googleapis.com" },
-      { rel: "preconnect", href: "https://fonts.gstatic.com" },
+      { rel: "preconnect", href: "https://fonts.bunny.net" },
       {
         rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css2?family=Nunito:wght@200;400;600;700&display=swap",
+        href: "https://fonts.bunny.net/css?family=nunito:200,400,600,700",
       },
       { rel: "icon", type: "image/x-icon", href: "favicon.ico" },
     ],
